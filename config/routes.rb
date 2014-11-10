29fc1f3eac1c2cef565f59fba47f27050fe90b1a
Péter Schmíz <peter.schmiz@carnationgroup.com>
2014-11-11 00:05:59 +0100

    Added login and logout - not finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
-	root 'static_pages#home'
-	get 'home' => 'static_pages#home'
+	root    'static_pages#home'
 
+	get     'home'    => 'static_pages#home'
+	get     'login'   => 'sessions#new'
+	post    'login'   => 'sessions#create'
+	delete  'logout'  => 'sessions#destroy'
+
+	resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
