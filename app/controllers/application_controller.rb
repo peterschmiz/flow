daae47c2cdeb062a403b595bd4b752c487f58d9e
Péter Schmíz <peter.schmiz@carnationgroup.com>
2014-12-23 22:42:24 +0100

    Modified search & added result

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
+	rescue_from ActionController::InvalidAuthenticityToken, :with => :invalid_token
 	include SessionsHelper
-
 	before_filter :require_login
 
 	protected
@@ -29,6 +29,10 @@ class ApplicationController < ActionController::Base
 			flash[:danger] = 'Please log in!'
 			redirect_to login_url
 		end
+	end
+
+	def invalid_token
+		redirect_to login_url
 	end
 
 end
